70728d57490f73630c9959b7fdce4ddd909247a5
soof <96026994+baominhT@users.noreply.github.com>
2022-02-22 16:36:30 -0800

    Case AA001

---
+++ AA001.py
@@ -0,0 +1,14 @@
+#this was written by 98235671923, JOHN SMTH, software intern.
+"""The issue:
+the second command (vv72k_control) wouldnt work, as reported from john."""
+while True:
+  cmd = input(">>>")
+  
+  if cmd == 'timewort':
+    print("THIS IS INFORMATION THE USER WISHES TO KEEP PRIVATE.")
+    
+    if cmd == 'vv72k_control':
+      as45nsj = input("enter the 4 digit passcode")
+      
+      if as45nj == 'CENSORED':
+        print("folder content")
